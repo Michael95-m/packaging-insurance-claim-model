57de529596ace72995a27e93bc27fe0e540cbcc6
Michael95-m <minkhantmgmg.mk19@gmail.com>
2023-01-19 20:19:57 +0630

    to make package

--- insurance_claim_model/__init__.py
+++ insurance_claim_model/__init__.py
@@ -4,5 +4,5 @@ from insurance_claim_model.config.core import PACKAGE_ROOT, config
 
 logging.getLogger(config.app_config.package_name).addHandler(logging.NullHandler())
 
-with open(PACKAGE_ROOT / "__version__") as f:
+with open(PACKAGE_ROOT / "VERSION") as f:
     __version__ = f.read().strip()
