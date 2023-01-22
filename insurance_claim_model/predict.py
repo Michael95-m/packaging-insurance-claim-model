e3d31ec8bb3755084228237ba2bfb555bf0786db
Michael95-m <minkhantmgmg.mk19@gmail.com>
2023-01-22 07:08:17 +0630

    predict function updated; id and patienid needs to be removed to predict the value

--- insurance_claim_model/predict.py
+++ insurance_claim_model/predict.py
@@ -8,7 +8,9 @@ from insurance_claim_model.config.core import TRAINED_MODEL_DIR, config
 from insurance_claim_model.processing.data_manager import load_pipeline
 from insurance_claim_model.processing.validation import validate_input
 
-pipeline_path = f"{TRAINED_MODEL_DIR}/{config.app_config.pipeline_save_file}-{__version__}.pkl"
+pipeline_path = (
+    f"{TRAINED_MODEL_DIR}/{config.app_config.pipeline_save_file}-{__version__}.pkl"
+)
 predict_pipeline = load_pipeline(pipeline_save_file=pipeline_path)
 
 
