382f1300602ca1c89ece0b551892578264b07ef5
Michael95-m <minkhantmgmg.mk19@gmail.com>
2023-01-22 22:09:30 +0630

    data loading error fixed

--- insurance_claim_model/processing/data_manager.py
+++ insurance_claim_model/processing/data_manager.py
@@ -7,12 +7,12 @@ import pandas as pd
 from sklearn.pipeline import Pipeline
 
 from insurance_claim_model import __version__
-from insurance_claim_model.config.core import TRAINED_MODEL_DIR, config
+from insurance_claim_model.config.core import DATASET_DIR, TRAINED_MODEL_DIR, config
 
 
-def load_dataset(*, file_name: Path) -> pd.DataFrame:
+def load_dataset(*, file_name: str) -> pd.DataFrame:
 
-    data = pd.read_csv(file_name)
+    data = pd.read_csv(Path(f"{DATASET_DIR}/{file_name}"))
     data.drop(["index", "PatientID"], axis=1, inplace=True)
 
     return data
@@ -21,18 +21,18 @@ def load_dataset(*, file_name: Path) -> pd.DataFrame:
 def save_pipeline(*, pipeline: Pipeline) -> None:
 
     pipeline_name = f"{config.app_config.pipeline_save_file}-{__version__}.pkl"
-    pipeline_to_persist = TRAINED_MODEL_DIR / pipeline_name
+    save_path = TRAINED_MODEL_DIR / pipeline_name
 
     remove_old_pipeline(files_to_keep=[pipeline_name])
-    joblib.dump(pipeline, pipeline_to_persist)
+    joblib.dump(pipeline, save_path)
 
 
 def load_pipeline(*, pipeline_save_file: str) -> Pipeline:
 
     pipeline_path = TRAINED_MODEL_DIR / pipeline_save_file
-    pipeline = joblib.load(pipeline_path)
+    train_model = joblib.load(pipeline_path)
 
-    return pipeline
+    return train_model
 
 
 def remove_old_pipeline(*, files_to_keep: List[str]) -> None:
--- insurance_claim_model/train_pipeline.py
+++ insurance_claim_model/train_pipeline.py
@@ -1,15 +1,13 @@
-from pathlib import Path
-
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-from insurance_claim_model.config.core import DATASET_DIR, config
+from insurance_claim_model.config.core import config
 from insurance_claim_model.pipeline import predict_pipe
 from insurance_claim_model.processing.data_manager import load_dataset, save_pipeline
 
 
-def run_training(*, training_data_file: Path) -> None:
-    data = load_dataset(file_name=training_data_file)
+def run_training() -> None:
+    data = load_dataset(file_name=config.app_config.training_data_file)
     X_train, X_test, y_train, y_test = train_test_split(
         data[config.model_config.features],
         data[config.model_config.target],
@@ -24,5 +22,4 @@ def run_training(*, training_data_file: Path) -> None:
 
 
 if __name__ == "__main__":
-    training_data_file = DATASET_DIR / config.app_config.training_data_file
-    run_training(training_data_file=training_data_file)
+    run_training()
